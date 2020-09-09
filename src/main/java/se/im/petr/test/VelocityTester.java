3d7f8c6336ee6541b11da4efac76df3462ae5b96
petr <petertq@gmail.com>
2020-09-09 12:22:19 +0200

    Velocity template fixes

--- src/main/java/se/im/petr/test/VelocityTester.java
+++ src/main/java/se/im/petr/test/VelocityTester.java
@@ -9,9 +9,12 @@ import java.util.Properties;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.jdom2.Content;
+import org.jdom2.Content.CType;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
+import org.jdom2.Text;
 import org.jdom2.input.SAXBuilder;
 
 public class VelocityTester {
@@ -36,15 +39,45 @@ public class VelocityTester {
 		System.out.println("transformed message:\n" + message);
 	}
 
+	public String spaces(int level) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < level; i++) {
+			sb.append("  ");
+		}
+		return sb.toString();
+	}
+	
+	public void listContent(List<Content> contentList, int level) {
+		for (Content content : contentList) {
+			CType ctype = content.getCType();
+			switch (ctype) {
+			case Element:
+				System.out.println(spaces(level) + "Element: " + content.toString());
+				if (content instanceof Element) {
+					Element element = (Element) content;
+					listContent(element.getContent(), level+1);
+				}
+				break;
+			case Text:
+				if (content instanceof Text) {
+					Text text = (Text) content;
+					System.out.println(spaces(level) + "Text: " + text.getText().trim());
+				}
+				break;
+			}
+		}
+	}
 	public void test2() throws Exception {
 		VelocityContext context = new VelocityContext();
 		SAXBuilder builder = new SAXBuilder();
 		try (InputStream is = this.getClass().getClassLoader().getResourceAsStream("AXESP0104.xml")) {
 			Document document = builder.build(is);
+			List<Content> contentList = document.getContent();
+//			listContent(contentList, 0);
 			Element root = document.getRootElement();
 			Namespace ns = Namespace.getNamespace("http://www.aftobbladet.se");
 			context.put("root", root);
-			context.put("ns", ns);
+			context.put("ns", null);
 			String message = createMessage("transform.vm", context);
 			System.out.println("transformed message:\n" + message);
 		}
@@ -53,6 +86,11 @@ public class VelocityTester {
 	public void test3() throws Exception {
 		JMSReceiver receiver = new JMSReceiver("midas", 5311, "edi");
 		String receivedMessage = receiver.receiveMessage(10);
+		System.out.println("received message: " + receivedMessage);
+		if (receivedMessage == null) {
+			System.out.println("no message");
+			return;
+		}
 		VelocityContext context = new VelocityContext();
 		SAXBuilder builder = new SAXBuilder();
 		Document document = builder.build(new StringReader(receivedMessage));
@@ -65,6 +103,6 @@ public class VelocityTester {
 	}
 
 	public static void main(String[] args) throws Exception {
-		new VelocityTester().test3();
+		new VelocityTester().test2();
 	}
 }
